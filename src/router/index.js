326ab435138ccf37531648b7c549e09f49c3ecfc
Mustafa <mustafa.kachwala@noborderz.com>
2022-06-12 02:33:13 +0530

    post page Ui create

--- src/router/index.js
+++ src/router/index.js
@@ -4,14 +4,16 @@ import Dashboard from "../pages/dashboard/index.js";
 import Login from "../pages/auth/login.js";
 import ResetPassword from "../pages/auth/reset-password.js";
 import UserList from "../pages/pageContainer/user-list";
+import PostList from "../pages/pageContainer/post.js";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={"/login"} element={<Login/>} />
+        <Route path={""} element={<Login/>} />
         <Route path={"/dashboard"} element={<Dashboard/>} />
         <Route path={"/userList"} element={<UserList/>} />
+        <Route path={"/post"} element={<PostList/>} />
         <Route path={"/reset-Password"} element={<ResetPassword/>} />
         <Route path="*" element={<>Not found</>} />
       </Routes>
