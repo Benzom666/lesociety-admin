e0a4ffc6bf3cfa052d0c3e4299d4fd3a5989d762
Mustafa <mustafa.kachwala@noborderz.com>
2022-06-15 04:03:24 +0530

    user modal and popup UI

--- src/pages/pageContainer/userTable.js
+++ src/pages/pageContainer/userTable.js
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { Form, InputGroup, DropdownButton, Dropdown  } from 'react-bootstrap';
+import { Form, InputGroup, DropdownButton, Dropdown, Button, Toast, Modal, ListGroup  } from 'react-bootstrap';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 const UserTableData = props => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [showA, setShowA] = useState(true);
+  const toggleShowA = () => setShowA(!showA);
   const products = [
     {id: 1, name: <div className='userNameImage'> <img src="https://i.ibb.co/JvCy0kr/RyanUser.png" alt="RyanUser" border="0"/>  Ryan Johnston </div> , gender: 'Male', registerDate:'26.07.2021', emailId: 'Oran.Olson94@hotmail.com', statusId: <span className='greenTxt'> Verified </span>, DropDown: <img src="https://i.ibb.co/jwq9z0R/moreIcon.png" alt="moreIcon" border="0"/>  },
     {id: 2, name: <div className='userNameImage'> <img src="https://i.ibb.co/JvCy0kr/RyanUser.png" alt="RyanUser" border="0"/>  Anna Adams </div> , gender: 'Female', registerDate:'23.07.2021', emailId: 'Gerda27@yahoo.com', statusId: <span className='greenTxt'> Verified </span>, DropDown: <img src="https://i.ibb.co/jwq9z0R/moreIcon.png" alt="moreIcon" border="0"/> },
@@ -75,6 +82,71 @@ const UserTableData = props => {
         defaultSorted={ defaultSorted } 
         selectRow={ selectRow }
       />
+       {/* <Button onClick={toggleShowA} className="mb-2">
+          Toggle Toast <strong>with</strong> Animation
+        </Button> */}
+        <Toast show={showA} onClose={toggleShowA} className="requestPopup">
+          <Toast.Header>
+          </Toast.Header>
+          <Toast.Body className='d-flex align-items-center w-100'>
+            <Form.Check 
+              type='checkbox'
+              label='people'
+              checked
+            />
+            <Button className='requestBtn'  onClick={handleShow}>Request</Button>
+            <Button className='verifyBtn' >verify</Button>
+          </Toast.Body>
+        </Toast>
+        <Modal show={show} onHide={handleClose} 
+            aria-labelledby="contained-modal-title-vcenter"
+            centered 
+            className='requestModal'>
+            <Modal.Header closeButton>           
+              <Modal.Title> Request Task</Modal.Title>
+              <p> Select an Option </p>
+            </Modal.Header>
+            <Modal.Body>
+              <ListGroup>
+                <ListGroup.Item>
+                  <Form.Check 
+                    type='radio'
+                    id='radio1'
+                    label="Cras justo odio"
+                  /> 
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Form.Check 
+                    type='radio'
+                    id='radio2'
+                    label="Dapibus ac facilisis in"
+                  /> 
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Form.Check 
+                    type='radio'
+                    id='radio3'
+                    label="Morbi leo risus"
+                  /> 
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Form.Check 
+                    type='radio'
+                    id='radio4'
+                    label="Porta ac consectetur ac"
+                  /> 
+                </ListGroup.Item>
+                <ListGroup.Item>
+                  <Form.Check 
+                    type='radio'
+                    id='radio5'
+                    label="Vestibulum at eros"
+                  /> 
+                </ListGroup.Item>
+              </ListGroup>
+              <Button className='verifyBtn' >verify</Button>
+            </Modal.Body>          
+        </Modal>
 		</>
 	)
 }
