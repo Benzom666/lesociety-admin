727435d5f90be3f3b105ce5b22cbd0dffb9f3419
Rohit Khokhar <rohitkhokharrk123@gmail.com>
2023-07-06 22:18:04 +0530

    8. in user list updated details have small issue we need to check in last i will show you with example how the flow works
    8. in user list updated details have small issue we need to check in last i will show you with example how the flow works

--- src/pages/UserList/UserTable.js
+++ src/pages/UserList/UserTable.js
@@ -111,7 +111,148 @@ function UserTable({ lastPostElementRef, endUser, status, noAction = false }) {
           </tr>
         </thead>
         <tbody>
-          {Array.isArray(userlist) && userlist.length
+          {status === 10 ?
+          Array.isArray(userlist) && userlist.length
+          ? userlist.map((user, index) => {
+            return (
+              (user?.un_verified_tagline || user?.un_verified_images?.length !== 0 || user?.un_verified_description) &&
+              <tr
+                key={user.id}
+                ref={
+                  userlist.length === index + 1 ? lastPostElementRef : null
+                }
+              >
+                <td>
+                  { !user?.request_change_fired && user?.email_verified && user.status === 1 && !noAction ? (
+                    <input
+                      id="user-checkbox"
+                      type="checkbox"
+                      onChange={checkboxHandler}
+                      value={user.email}
+                      name="user-checkbox"
+                      checked={rowSelected.includes(user.email)}
+                    />
+                  ) : null}
+                </td>
+                <td>
+                  <div className="userNameImage" key={index}>
+                    <Link
+                      to={"/profile/" + user.user_name}
+                      onClick={(e) => {
+                        dispatch(getUserProfile(user.user_name));
+                      }}
+                    >
+                      <img
+                        src={user?.un_verified_images[0] || user?.images[0] || ProfileImage}
+                        alt="RyanUser"
+                        border="0"
+                      />{" "}
+                      <p> {user.user_name} </p>
+                    </Link>
+                  </div>
+                </td>
+                <td>{user?.gender}</td>
+                <td>{moment( status === 6 ? user?.requested_date : user?.created_at).format("DD.MM.YYYY")}</td>
+                <td>{user?.email}</td>
+                <td>
+                  {user?.email_verified ? (
+                    <p className="greenTxt">Verified </p>
+                  ) : (
+                    <p className="redTxt">Pending</p>
+                  )}
+                </td>
+                <td>
+                  {
+                    <span className="greenTxt">
+                      {user?.email_verified &&
+                        (
+                          user.request_change_fired ? <p className="redTxt">Requested</p>
+                            : user.status == 1 ? <p className="text-warning">Pending</p>
+                              : user.status == 2 ? <p className="greenTxt">Verified</p>
+                                : user.status == 3 ? <p className="redTxt">Block</p>
+                                  : ""
+                        )}
+                    </span>
+                  }
+                </td>
+                <td>
+                  {(user?.email_verified && (!user.request_change_fired && user.status === 1 ) && !noAction) || status === 10 ? (
+                    <DropdownButton
+                      variant="outline-secondary"
+                      title={
+                        <img
+                          src="https://i.ibb.co/jwq9z0R/moreIcon.png"
+                          alt="moreIcon"
+                          border="0"
+                        />
+                      }
+                      id="input-group-dropdown-2"
+                      align="end"
+                    >
+                      <Dropdown.Item
+                        eventKey="1"
+                        onClick={() => {
+                          dispatch(postUpdateUserStatus(2, user.email, "user-list", status));
+                          // dispatch(getUserList());
+                        }}
+                      >
+                        Verify
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        eventKey="req"
+                        onClick={() => {
+                          setShow(true);
+                          setUserEmail(user?.email);
+                          dispatch({
+                            type: Utils.ActionName.USER_LIST,
+                            payload: { rowSelected: [user?.email] },
+                          });
+                        }}
+                      >
+                        Request a Change
+                      </Dropdown.Item>
+                      <Dropdown.Item
+                        eventKey="3"
+                        onClick={() => {
+                          dispatch(postUpdateUserStatus(3, user.email, 'user-list', status));
+                        }}
+                      >
+                        Block
+                      </Dropdown.Item>
+                    </DropdownButton>
+                  ) 
+                  // : status === 10 ? (
+                  //   <DropdownButton
+                  //     variant="outline-secondary"
+                  //     title={
+                  //       <img
+                  //         src="https://i.ibb.co/jwq9z0R/moreIcon.png"
+                  //         alt="moreIcon"
+                  //         border="0"
+                  //       />
+                  //     }
+                  //     id="input-group-dropdown-2"
+                  //     align="end"
+                  //   >
+                  //     <Dropdown.Item
+                  //       eventKey="1"
+                  //       onClick={() => {
+                  //         dispatch(postVerfiyUser(user.email));
+                  //       }}
+                  //     >
+                  //       Verify
+                  //     </Dropdown.Item>
+                  //   </DropdownButton>
+                  // )
+                    : (
+                      ""
+                    )}
+                </td>
+              </tr>
+            );
+          })
+          : null :
+          Array.isArray(userlist) && userlist.length
             ? userlist.map((user, index) => {
               return (
                 <tr
@@ -249,7 +390,8 @@ function UserTable({ lastPostElementRef, endUser, status, noAction = false }) {
                 </tr>
               );
             })
-            : null}
+            : null
+            }
         </tbody>
       </Table>
       {!!rowSelected && rowSelected.length > 0 ? (
